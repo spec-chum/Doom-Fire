d806b091fb9e2ae78b91199d22f863bea490a83e
Spec-Chum <specchumster@gmail.com>
2024-10-12 19:16:12 +0100

    Bit of a refactor, nothing major

--- Program.cs
+++ Program.cs
@@ -12,48 +12,24 @@ internal static class Program
     const int WindowWidth = CanvasWidth * Scale;
     const int WindowHeight = CanvasHeight * Scale;
 
+    static readonly Color[] colors = [
+        new (  0,   0,   0, 255),  new ( 31,   7,   7, 255),  new ( 71,  15,   7, 255),
+        new (  7,   7,   7, 255),  new ( 47,  15,   7, 255),  new ( 87,  23,   7, 255),
+        new (103,  31,   7, 255),  new (119,  31,   7, 255),  new (143,  39,   7, 255),
+        new (159,  47,   7, 255),  new (175,  63,   7, 255),  new (191,  71,   7, 255),
+        new (199,  71,   7, 255),  new (223,  79,   7, 255),  new (223,  87,   7, 255),
+        new (223,  87,   7, 255),  new (215,  95,   7, 255),  new (215,  95,   7, 255),
+        new (215, 103,  15, 255),  new (207, 111,  15, 255),  new (207, 119,  15, 255),
+        new (207, 127,  15, 255),  new (207, 135,  23, 255),  new (199, 135,  23, 255),
+        new (199, 143,  23, 255),  new (199, 151,  31, 255),  new (191, 159,  31, 255),
+        new (191, 159,  31, 255),  new (191, 167,  39, 255),  new (191, 167,  39, 255),
+        new (191, 175,  47, 255),  new (183, 175,  47, 255),  new (183, 183,  47, 255),
+        new (183, 183,  55, 255),  new (207, 207, 111, 255),  new (223, 223, 159, 255),
+        new (239, 239, 199, 255)
+    ];
+
     static void Main()
     {
-        ReadOnlySpan<Color> colors = [
-            new Color(0, 0, 0, 255),
-            new Color(7, 7, 7, 255),
-            new Color(31, 7, 7, 255),
-            new Color(47, 15, 7, 255),
-            new Color(71, 15, 7, 255),
-            new Color(87, 23, 7, 255),
-            new Color(103, 31, 7, 255),
-            new Color(119, 31, 7, 255),
-            new Color(143, 39, 7, 255),
-            new Color(159, 47, 7, 255),
-            new Color(175, 63, 7, 255),
-            new Color(191, 71, 7, 255),
-            new Color(199, 71, 7, 255),
-            new Color(223, 79, 7, 255),
-            new Color(223, 87, 7, 255),
-            new Color(223, 87, 7, 255),
-            new Color(215, 95, 7, 255),
-            new Color(215, 95, 7, 255),
-            new Color(215, 103, 15, 255),
-            new Color(207, 111, 15, 255),
-            new Color(207, 119, 15, 255),
-            new Color(207, 127, 15, 255),
-            new Color(207, 135, 23, 255),
-            new Color(199, 135, 23, 255),
-            new Color(199, 143, 23, 255),
-            new Color(199, 151, 31, 255),
-            new Color(191, 159, 31, 255),
-            new Color(191, 159, 31, 255),
-            new Color(191, 167, 39, 255),
-            new Color(191, 167, 39, 255),
-            new Color(191, 175, 47, 255),
-            new Color(183, 175, 47, 255),
-            new Color(183, 183, 47, 255),
-            new Color(183, 183, 55, 255),
-            new Color(207, 207, 111, 255),
-            new Color(223, 223, 159, 255),
-            new Color(239, 239, 199, 255)
-        ];
-
         Raylib.InitWindow(WindowWidth, WindowHeight, "Fire");
         Raylib.SetTargetFPS(30);
 
@@ -66,13 +42,13 @@ internal static class Program
         Span<Color> framebuffer;
         unsafe
         {
-            framebuffer = new((Color *)canvas.Data, buffer.Length);
+            framebuffer = new((Color*)canvas.Data, buffer.Length);
         }
 
         while (!Raylib.WindowShouldClose())
         {
             DoFire(buffer);
-            UpdateFramebuffer(framebuffer, buffer, colors);
+            UpdateFramebuffer(framebuffer, buffer);
 
             Raylib.BeginDrawing();
 
@@ -86,7 +62,7 @@ internal static class Program
         Raylib.UnloadTexture(texture);
     }
 
-    static void UpdateFramebuffer(Span<Color> framebuffer, ReadOnlySpan<int> buffer, ReadOnlySpan<Color> colors)
+    static void UpdateFramebuffer(Span<Color> framebuffer, ReadOnlySpan<int> buffer)
     {
         for (int i = 0; i < framebuffer.Length; i++)
         {
